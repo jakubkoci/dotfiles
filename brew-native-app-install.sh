0579972d858dcfbfeb220c6e4499827012401473
Jakub Koci <jakub.koci@gmail.com>
2025-12-03 07:43:53 +0100

    Update native apps list

--- brew-native-app-install.sh
+++ brew-native-app-install.sh
@@ -2,26 +2,26 @@
 
 # Install native apps
 
-brew install --cask brave-browser
-brew install --cask todoist
 brew install --cask iterm2
+brew install --cask brave-browser
+brew install --cask obsidian
 brew install --cask google-chrome
 brew install --cask google-drive
-brew install --cask flux
 brew install --cask appcleaner
+brew install --cask flux
 brew install --cask moom
+brew install --cask betterdisplay
+brew install --cask docker-desktop
 brew install --cask visual-studio-code
-brew install --cask android-studio
-brew install --cask rancher
+brew install --cask zed
+brew install --cask warp
+# brew install --cask android-studio
 brew install --cask figma
 brew install --cask postman
 brew install --cask slack
 brew install --cask discord
 brew install --cask spotify
 brew install --cask vlc
-brew install --cask betterdisplay
-# brew install --cask the-unarchiver
-# brew cask install licecap
 
 # cleanup
 brew cleanup
